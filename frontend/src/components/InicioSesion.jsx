281b9910ae1d74edebc8bb01791cce2f08dbb542
seism <dcalejandro667@proton.me>
2025-01-01 23:04:35 -0300

    check for institutional mail, format file

--- frontend/src/components/InicioSesion.jsx
+++ frontend/src/components/InicioSesion.jsx
@@ -6,85 +6,90 @@ import "./css/InicioSesion.css";
 import seguraLogo from "../assets/LogoSF.png";
 
 export default function InicioSesion() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-  const { setUser } = useContext(UserContext);
-  // Función para manejar el control de acceso
-  function control_de_acceso(event) {
-    // Evita que el formulario realice su acción predeterminada (recargar la página)
-    event.preventDefault();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+    const { setUser } = useContext(UserContext);
+    // Función para manejar el control de acceso
+    function control_de_acceso(event) {
+        // Evita que el formulario realice su acción predeterminada (recargar la página)
+        event.preventDefault();
 
-    // Envía una solicitud POST al servidor con las credenciales del usuario
-    fetch("http://localhost:3001/login", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          const { query_ustype, email } = data.result;
-          alert(`Bienvenido ${email}. Tipo de usuario: ${query_ustype}`);
-
-          setUser({ userType: query_ustype, email });
-          console.log("User set in context:", {
-            userType: query_ustype,
-            email,
-          });
-          if (query_ustype === "Administrador") {
-            // Si es administrador, navega a la página de mantenimiento de tablas
-            navigate("/adminspacework");
-          } else {
-            // Si no es administrador, navega a la página principal
-            navigate("/pagp");
-          }
-        } else {
-          // Muestra un mensaje de error si las credenciales son incorrectas
-          setError(data.message);
+        if (!email.endsWith("@usach.cl")) {
+            setError("Debe ingresar un correo institucional")
+            return
         }
-      });
-  }
 
-  return (
-    <div className="fondo_inicio_sesion">
-      <Cabecera />
-      <div className="fondo_login_interface">
-        <div className="fondo_form_and_logo">
-          <div className="fondo_form_login">
-            <form className="form_login" onSubmit={control_de_acceso}>
-              <h2>Iniciar Sesión</h2>
-              <input
-                type="email"
-                name="email"
-                value={email}
-                placeholder="Correo"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <input
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Contraseña"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              {error && <p className="error_feedback">{error}</p>}
-              <button type="submit"> Ingresar </button>
-              <a href="#">
-                <h3>Registrarse</h3>
-              </a>
-            </form>
-          </div>
-          <div className="fondo_logo_USegura">
-            <img src={seguraLogo} alt="S" className="logo_USegura" />
-          </div>
+        // Envía una solicitud POST al servidor con las credenciales del usuario
+        fetch("http://localhost:3001/login", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    const { query_ustype, email } = data.result;
+                    alert(`Bienvenido ${email}. Tipo de usuario: ${query_ustype}`);
+
+                    setUser({ userType: query_ustype, email });
+                    console.log("User set in context:", {
+                        userType: query_ustype,
+                        email,
+                    });
+                    if (query_ustype === "Administrador") {
+                        // Si es administrador, navega a la página de mantenimiento de tablas
+                        navigate("/adminspacework");
+                    } else {
+                        // Si no es administrador, navega a la página principal
+                        navigate("/pagp");
+                    }
+                } else {
+                    // Muestra un mensaje de error si las credenciales son incorrectas
+                    setError(data.message);
+                }
+            });
+    }
+
+    return (
+        <div className="fondo_inicio_sesion">
+            <Cabecera />
+            <div className="fondo_login_interface">
+                <div className="fondo_form_and_logo">
+                    <div className="fondo_form_login">
+                        <form className="form_login" onSubmit={control_de_acceso}>
+                            <h2>Iniciar Sesión</h2>
+                            <input
+                                type="email"
+                                name="email"
+                                value={email}
+                                placeholder="Correo"
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                            <input
+                                type="password"
+                                name="password"
+                                value={password}
+                                placeholder="Contraseña"
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                            {error && <p className="error_feedback">{error}</p>}
+                            <button type="submit"> Ingresar </button>
+                            <a href="#">
+                                <h3>Registrarse</h3>
+                            </a>
+                        </form>
+                    </div>
+                    <div className="fondo_logo_USegura">
+                        <img src={seguraLogo} alt="S" className="logo_USegura" />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
