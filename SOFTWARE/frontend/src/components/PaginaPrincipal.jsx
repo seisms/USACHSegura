335d2051581d543477bacedc5683232b6dbb5eed
seism <dcalejandro667@proton.me>
2025-01-06 03:59:20 -0300

    in PagP: route admin to admin page

--- SOFTWARE/frontend/src/components/PaginaPrincipal.jsx
+++ SOFTWARE/frontend/src/components/PaginaPrincipal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./BarraNavegacion";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import Cookies from "js-cookie"
 import MapaDefault from "./mapasEAO/MapaDefault";
 import CalcularIndiceSeguridad from "./Genericos/IndiceSeguridad";
@@ -11,86 +11,90 @@ import Report from "../assets/Re.jpg";
 import NotificarReporte from "./Genericos/NotificarReporte";
 
 export default function PagP() {
-	const [showReportForm, setShowReportForm] = useState(false);
-	const [submittedReport, setSubmittedReport] = useState(null);
-  const [reportID, setReportID] = useState(0);
-	const navigate = useNavigate()
-	const email = Cookies.get("username")
+    const [showReportForm, setShowReportForm] = useState(false);
+    const [submittedReport, setSubmittedReport] = useState(null);
+    const [reportID, setReportID] = useState(0);
+    const navigate = useNavigate()
+    const email = Cookies.get("username")
+    const usertype = Cookies.get("usertype");
 
-	const toggleReportForm = () => {
-		setShowReportForm(!showReportForm);
-	};
+    const toggleReportForm = () => {
+        setShowReportForm(!showReportForm);
+    };
 
-	const handleReportSubmit = (reportData) => {
-		setSubmittedReport(reportData);
-		setShowReportForm(false);
-	};
+    const handleReportSubmit = (reportData) => {
+        setSubmittedReport(reportData);
+        setShowReportForm(false);
+    };
 
-  const handleReportID = (id) => {
-    setReportID(id);
-  }
+    const handleReportID = (id) => {
+        setReportID(id);
+    }
 
-  const closeReportPopup = () => {
-    setSubmittedReport(null);
-  };
+    const closeReportPopup = () => {
+        setSubmittedReport(null);
+    };
 
-	const formatDate = (dateString) => {
-		const options = { day: "2-digit", month: "2-digit", year: "numeric" };
-		return new Date(dateString).toLocaleDateString("es-CL", options);
-	};
+    const formatDate = (dateString) => {
+        const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+        return new Date(dateString).toLocaleDateString("es-CL", options);
+    };
 
-  useEffect(() => {
-		if (email === undefined) {
-			navigate("/");
-		}
-	}, [email, navigate]); //
+    useEffect(() => {
+        if (email === undefined) {
+            navigate("/");
+        }
+        if (usertype === "Administrador") {
+            navigate("/adminspacework")
+        }
+    }, [email, navigate]); //
 
-  return (
-    <div className="fondo_pagp">
-      <Navbar />
-      <CalcularIndiceSeguridad />
-      <MapaSeccionado />
-      { /*<MapaSeccionado />*/}
-      <MapaDefault />
-      {showReportForm && <div className="overlay"></div>}
-      {showReportForm && (
-        <FormularioReporte
-          onClose={toggleReportForm}
-          onSubmit={handleReportSubmit}
-          onReportID={handleReportID}
-        />
-      )}
-      <NotificarReporte rep_id={reportID} />
-      <button className="report-button" onClick={toggleReportForm}>
-        <img src={Report} alt="Reporte" className="report-icon" />
-      </button>
-      {submittedReport && (
-        <div className="report-popup">
-          <button className="close-popup" onClick={closeReportPopup}>
-            X
-          </button>
-          <h2>Reporte Generado</h2>
-          <p>
-            <strong>Tipo De Incidente:</strong>{" "}
-            {submittedReport.tipo || "Ninguno"}
-          </p>
-          <p>
-            <strong>Pertenencia Perdida:</strong>{" "}
-            {submittedReport.perts.join(", ") || "Ninguna"}
-          </p>
-          <p>
-            <strong>Sector Del Incidente:</strong>{" "}
-            {submittedReport.sector || "Ninguno"}
-          </p>
-          <p>
-            <strong>Fecha:</strong>{" "}
-            {formatDate(submittedReport.fecha) || "No especificada"}
-          </p>
-          <p>
-            <strong>Hora:</strong> {submittedReport.hora || "No especificada"}
-          </p>
+    return (
+        <div className="fondo_pagp">
+            <Navbar />
+            <CalcularIndiceSeguridad />
+            <MapaSeccionado />
+            { /*<MapaSeccionado />*/}
+            <MapaDefault />
+            {showReportForm && <div className="overlay"></div>}
+            {showReportForm && (
+                <FormularioReporte
+                    onClose={toggleReportForm}
+                    onSubmit={handleReportSubmit}
+                    onReportID={handleReportID}
+                />
+            )}
+            <NotificarReporte rep_id={reportID} />
+            <button className="report-button" onClick={toggleReportForm}>
+                <img src={Report} alt="Reporte" className="report-icon" />
+            </button>
+            {submittedReport && (
+                <div className="report-popup">
+                    <button className="close-popup" onClick={closeReportPopup}>
+                        X
+                    </button>
+                    <h2>Reporte Generado</h2>
+                    <p>
+                        <strong>Tipo De Incidente:</strong>{" "}
+                        {submittedReport.tipo || "Ninguno"}
+                    </p>
+                    <p>
+                        <strong>Pertenencia Perdida:</strong>{" "}
+                        {submittedReport.perts.join(", ") || "Ninguna"}
+                    </p>
+                    <p>
+                        <strong>Sector Del Incidente:</strong>{" "}
+                        {submittedReport.sector || "Ninguno"}
+                    </p>
+                    <p>
+                        <strong>Fecha:</strong>{" "}
+                        {formatDate(submittedReport.fecha) || "No especificada"}
+                    </p>
+                    <p>
+                        <strong>Hora:</strong> {submittedReport.hora || "No especificada"}
+                    </p>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
